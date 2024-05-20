3cb98e12c0c0cdd41b34d2999f29add12fbd6c69
agaberr <ahmedgaber312001@gmail.com>
2024-05-20 07:54:15 +0000

    install another package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.0.3',  # Use a compatible version with Flask 2.1.0
+  provider => 'pip3',
+}
