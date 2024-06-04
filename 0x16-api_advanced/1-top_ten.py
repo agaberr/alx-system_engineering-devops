dae7460ff083cbc41034ddedd4e1ebe448bf5a2a
agaberr <ahmedgaber312001@gmail.com>
2024-06-04 18:11:41 +0000

    tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,15 +11,11 @@ def top_ten(subreddit):
     posts listed for a given subreddit'''
 
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
     params = {
         "limit": 10
     }
 
-    response = requests.get(url, allow_redirects=False, params=params,
-                            headers=headers)
+    response = requests.get(url, allow_redirects=False, params=params)
 
     if response.status_code != 200:
         print("None")
