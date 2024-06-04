98ab9c4802ae1037af796fd67273fc72bcdbbf18
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-06-04 20:59:28 +0300

    tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Return the total number of subscribers on a given subreddit.'''
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    response = requests.get(url, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    results = response.json().get('data')
+    return results.get("subscribers")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit
+'''
+import requests
+
+
+def top_ten(subreddit):
+    ''' prints the titles of the first 10 hot posts listed for a given subreddit'''
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(url, allow_redirects=False, params=params)
+
+    if response.status_code != 200:
+        print("None")
+        return
+    results = response.json().get('data')
+    [print(c.get("data").get("title")) for c in results.get("children")]
