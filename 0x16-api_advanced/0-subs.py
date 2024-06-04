f5a4217fd337893fb5b3d38278fea8560b1700ce
agaberr <ahmedgaber312001@gmail.com>
2024-06-04 18:09:12 +0000

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,10 @@ def number_of_subscribers(subreddit):
     '''Return the total number of subscribers on a given subreddit.'''
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    response = requests.get(url, allow_redirects=False)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, allow_redirects=False, headers=headers)
 
     if response.status_code != 200:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,12 +11,15 @@ def top_ten(subreddit):
     posts listed for a given subreddit'''
 
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
         "limit": 10
     }
 
-    response = requests.get(url, allow_redirects=False, params=params)
+    response = requests.get(url, allow_redirects=False, params=params,
+                            headers=headers)
 
     if response.status_code != 200:
         print("None")
