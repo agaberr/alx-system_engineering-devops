c28153c37fa686797b6374d4b7df2eb981b2d861
agaberr <ahmedgaber312001@gmail.com>
2024-06-05 19:50:32 +0000

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,10 @@
-# Apache error 500 fix
+# A puppet line in a file
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
